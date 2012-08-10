494e5803febf15e60a9dc1e4eb224879e0184bff
Helder Santana <helder.css@gmail.com>
2012-08-10 02:17:45 -0300

    composer lock must be generated

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -5,29 +5,21 @@ if (!class_exists('Composer\\Autoload\\ClassLoader', false)) {
     require __DIR__ . '/composer' . '/ClassLoader.php';
 }
 
-if (!class_exists('ComposerAutoloaderInit', false)) {
-    class ComposerAutoloaderInit
-    {
-        public static function getLoader()
-        {
-            $loader = new \Composer\Autoload\ClassLoader();
-            $composerDir = __DIR__ . '/composer';
+return call_user_func(function() {
+    $loader = new \Composer\Autoload\ClassLoader();
+    $composerDir = __DIR__ . '/composer';
 
-            $map = require $composerDir . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->add($namespace, $path);
-            }
-
-            $classMap = require $composerDir . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-
-            $loader->register();
-
-            return $loader;
-        }
+    $map = require $composerDir . '/autoload_namespaces.php';
+    foreach ($map as $namespace => $path) {
+        $loader->add($namespace, $path);
     }
-}
 
-return ComposerAutoloaderInit::getLoader();
+    $classMap = require $composerDir . '/autoload_classmap.php';
+    if ($classMap) {
+        $loader->addClassMap($classMap);
+    }
+
+    $loader->register();
+
+    return $loader;
+});
